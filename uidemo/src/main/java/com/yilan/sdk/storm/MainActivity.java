e5f0f43ce4e438db5a9bfc58ccfbc2b5b76e433b
lihu <lihu@yilan.tv>
2019-08-22 16:31:13 +0800

    修改版本为1.3.2.1

--- uidemo/src/main/java/com/yilan/sdk/storm/MainActivity.java
+++ uidemo/src/main/java/com/yilan/sdk/storm/MainActivity.java
@@ -1,8 +1,12 @@
 package com.yilan.sdk.storm;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 
 //import com.yilan.sdk.ui.configs.FeedConfig;
@@ -13,51 +17,77 @@ import com.yilan.sdk.ui.category.ChannelFragment;
 import com.yilan.sdk.ui.feed.FeedFragment;
 import com.yilan.sdk.ui.littlevideo.LittleVideoFragment;
 
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private View mDecorView;
-    private FeedFragment feedFragment;
+    private FragmentManager manager;
+    private ChannelFragment mChannelFragment;
+    private LittleVideoFragment mLittleVideoFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.scroll).setOnClickListener(new View.OnClickListener() {
+
+        manager = getSupportFragmentManager();
+        final BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.post(new Runnable() {
             @Override
-            public void onClick(View v) {
-                if (feedFragment != null) {
-                    feedFragment.scrollToTop();
-                }
+            public void run() {
+                navigation.setSelectedItemId(R.id.navigation_home);
             }
         });
-
-        FragmentManager manager = getSupportFragmentManager();
-        //带频道导航栏
-
-        ChannelFragment fragment = new ChannelFragment();
-        manager.beginTransaction().replace(R.id.content, fragment).commitAllowingStateLoss();
-
-        //单个频道
-//        Channel channel = new Channel();
-//        channel.setId("1291");
-//
-//        feedFragment = FeedFragment.newInstance(channel);
-//        manager.beginTransaction().replace(R.id.content, feedFragment).commitAllowingStateLoss();
-
-
-        //小视频
-//        LittleVideoFragment littleVideoFragment = LittleVideoFragment.newInstance();
-//        manager.beginTransaction().replace(R.id.content, littleVideoFragment).commitAllowingStateLoss();
     }
 
-    private void hideSystemUI() {
-        mDecorView = getWindow().getDecorView();
-        mDecorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_IMMERSIVE);
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    hideAll();
+                    if (mChannelFragment != null) {
+                        manager.beginTransaction().show(mChannelFragment).commitAllowingStateLoss();
+                    } else {
+                        mChannelFragment = new ChannelFragment();
+                        manager.beginTransaction().add(R.id.content, mChannelFragment, ChannelFragment.class.getSimpleName()).commitAllowingStateLoss();
+                    }
+                    return true;
+                case R.id.navigation_little:
+                    hideAll();
+                    if (mLittleVideoFragment != null) {
+                        manager.beginTransaction().show(mLittleVideoFragment).commitAllowingStateLoss();
+                    } else {
+                        mLittleVideoFragment = new LittleVideoFragment();
+                        manager.beginTransaction().add(R.id.content, mLittleVideoFragment, LittleVideoFragment.class.getSimpleName()).commitAllowingStateLoss();
+                    }
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void hideAll() {
+        List<Fragment> fragmentList = manager.getFragments();
+        for (Fragment fragment : fragmentList) {
+            manager.beginTransaction().hide(fragment).commitAllowingStateLoss();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        boolean canback = true;
+        if (mChannelFragment != null && mChannelFragment.isVisible()) {
+            canback = mChannelFragment.canBack();
+        }
+        if (!canback) {
+            return;
+        } else {
+            super.onBackPressed();
+        }
     }
 }
--- uidemo/src/main/java/com/yilan/sdk/storm/UIDemoApplication.java
+++ uidemo/src/main/java/com/yilan/sdk/storm/UIDemoApplication.java
@@ -5,8 +5,16 @@ import android.content.Context;
 import android.util.Log;
 
 import com.yilan.sdk.entity.MediaInfo;
+import com.yilan.sdk.player.UserCallback;
+import com.yilan.sdk.player.entity.PlayData;
+import com.yilan.sdk.player.utils.Constant;
 import com.yilan.sdk.ui.YLUIInit;
+import com.yilan.sdk.ui.ad.entity.AdEntity;
+import com.yilan.sdk.ui.configs.AdVideoCallback;
 import com.yilan.sdk.ui.configs.FeedConfig;
+import com.yilan.sdk.ui.configs.LittleVideoConfig;
+import com.yilan.sdk.ui.configs.PlayerConfig;
+import com.yilan.sdk.uibase.ui.web.WebActivity;
 
 public class UIDemoApplication extends Application {
     private String TAG = UIDemoApplication.class.getSimpleName();
@@ -18,42 +26,59 @@ public class UIDemoApplication extends Application {
         //初始化
         YLUIInit.getInstance()
                 .setApplication(this)
-                .setAccessKey("")
-                .setAccessToken("")
-                .setUid("uisdk id")
+//                .setUid("uisdk id")
                 .build();
 
         //Feed流页面配置
-//        FeedConfig.getInstance()
-//                .setPlayerStyle(FeedConfig.STYLE_NATIVE)
+        FeedConfig.getInstance()
+                .setPlayerStyle(FeedConfig.STYLE_NATIVE);
 //                .setViewHolder(new FeedStyleViewHolder())
 //                .setOnItemClickListener(new FeedConfig.OnClickListener() {
 //                    @Override
 //                    public boolean onClick(Context context, MediaInfo info) {
 //                        Log.e(TAG, "点击了 " + info.getH5_url());
 //                        WebActivity.start(context, info.getH5_url(),"");
-//                        return false;
+//                        return true;
 //                    }
 //                });
         //播放页配置
-//        PlayerConfig.getInstance().setPlayerStyle(PlayerConfig.STYLE_NATIVE_FEED);
-        //小视频页面配置
+//        小视频页面配置
 //        LittleVideoConfig.getInstance()
-//                .setViewHolder(new TestLittleVideoViewHolder())
-//                .setPlayerCallback(new UserCallback() {
-//                    @Override
-//                    public void event(int type, PlayData data, int playerHash) {
-//                        switch (type) {
-//                            case Constant.STATE_PREPARED:
-//                            case Constant.STATE_ERROR:
-//                            case Constant.STATE_PLAYING:
-//                            case Constant.STATE_COMPLETE:
-//                            case Constant.STATE_PAUSED:
-//                                Log.e(TAG, "播放器状态" + type);
-//                                break;
-//                        }
-//                    }
-//                });
+//                .setViewHolder(new TestLittleVideoViewHolder());
+
+
+        //设置小视频的视频广告回调监听
+        LittleVideoConfig.getInstance().setAdVideoCallback(new AdVideoCallback() {
+            @Override
+            public void onVideoLoad(AdEntity entity) {
+                Log.d("adadad", "视频加载成功" + entity.getAdSlotId());
+            }
+
+            @Override
+            public void onVideoError(int errorCode, AdEntity entity) {
+                Log.d("adadad", "视频播放错误：errorCode=" + errorCode + entity.getAdSlotId());
+            }
+
+            @Override
+            public void onVideoAdStartPlay(AdEntity entity) {
+                Log.d("adadad", "视频开始播放" + entity.getAdSlotId());
+            }
+
+            @Override
+            public void onVideoAdPaused(AdEntity entity) {
+                Log.d("adadad", "视频暂停播放" + entity.getAdSlotId());
+            }
+
+            @Override
+            public void onVideoAdContinuePlay(AdEntity entity) {
+                Log.d("adadad", "视频继续播放" + entity.getAdSlotId());
+            }
+        });
+
+        //设置评论类型
+//        PlayerConfig.getInstance().setCommentType(PlayerConfig.DISMISS_COMMENT);
+        PlayerConfig.getInstance().setCommentType(PlayerConfig.SHOW_COMMENT_LIST);
+//        PlayerConfig.getInstance().setCommentType(PlayerConfig.SHOW_COMMENT_ALL);
 
     }
 }
